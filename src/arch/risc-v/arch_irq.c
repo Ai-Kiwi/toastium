dc70ab90a5ace43212fe3f0c3351abdd859d742d
Ai Kiwi <contact@aikiwi.dev>
2026-04-08 23:22:24 +1200

    added handled unknown irq, made threshold setting run on init instead of every enable

--- src/arch/risc-v/arch_irq.c
+++ src/arch/risc-v/arch_irq.c
@@ -1,5 +1,6 @@
 #include "./../../kernel/irq.h"
 #include "../../drivers/uart/uart.h"
+#include "panic.h"
 
 //these values are S mode
 #define PLIC_BASE 0x0C000000
@@ -11,13 +12,16 @@
 
 
 void arch_enable_irq(IRQ_TYPES irq_type, int priority) {
-    *(volatile int*)(PLIC_BASE + 10 * 4) = 1; //set lowest priority for uart
-    *(volatile int*)(PLIC_BASE + 0x2080) |= (1U << 10); //set interrupt for uart
-
-    *(volatile int*)(PLIC_threshold) = 0;
+    if (irq_type == UART) {
+        *(volatile int*)(PLIC_BASE + 10 * 4) = 1; //set lowest priority for uart
+        *(volatile int*)(PLIC_BASE + 0x2080) |= (1U << 10); //set interrupt for uart
+    }else{
+        panic("ATTEMPT_TO_ENABLE_UNHANDLED_IRQ",irq_type);
+    }
 }
 
 void arch_irq_init() {
     asm volatile("csrs sie, %0" :: "r"(1 << 9)); //enable external traps (bit 10 on)
     asm volatile("csrs sstatus, %0" :: "r"(1 << 1)); //enable global interrupts (bit 3 on)
+    *(volatile int*)(PLIC_threshold) = 0; //set threshold to trigger trap
 }
