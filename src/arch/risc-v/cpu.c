6525c5a66e35a7f7afd9fd5fcf46888e6343e49f
Ai Kiwi <contact@aikiwi.dev>
2026-04-08 23:27:56 +1200

    Fixed few mistakes in freeze code

--- src/arch/risc-v/cpu.c
+++ src/arch/risc-v/cpu.c
@@ -1,6 +1,6 @@
 void freeze_system() {
-    asm volatile("csrci sstatus, 8"); //disable traps
+    asm volatile ("csrc sstatus, %0" :: "r"(1 << 1)); //disable traps
     while (1){
-        asm("wfi");
+        asm volatile("wfi");
     }
 }
