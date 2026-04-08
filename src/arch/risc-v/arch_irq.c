f2e58146cf919f57607264d1061426149056a887
Ai Kiwi <contact@aikiwi.dev>
2026-04-08 21:54:32 +1200

    Removed unneeded logging

--- src/arch/risc-v/arch_irq.c
+++ src/arch/risc-v/arch_irq.c
@@ -4,5 +4,4 @@
 void arch_enable_irq(IRQ_TYPES irq_type, int priority) {
     *(volatile int*)(0x0C002000) |= (1U << 10); //set interrupt for uart
     *(volatile int*)(0x0C001000 + 10 * 4) = 1; //set lowest priority for uart    
-    uart_print_chars("Arch enable irq\n");
 }
