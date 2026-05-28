035742121f26fd89441595f7a8dd5c689d5e9cb3
Ai Kiwi <contact@aikiwi.dev>
2026-05-28 17:38:36 +1200

    Added page range type

--- src/kernel/memory/pager.h
+++ src/kernel/memory/pager.h
@@ -1,6 +1,11 @@
 #ifndef KERNEL_PAGER_H
 #define KERNEL_PAGER_H
 
+typedef struct {
+    u64 page_start;
+    u64 page_size;
+} page_range;
+
 void kernel_pager_init();
 u64 kernel_pager_acquire();
 void kernel_pager_release(u64 location);
