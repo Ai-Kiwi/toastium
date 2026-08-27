1f92fd58917dfb134b37d4eb67da5d50c190d789
Ai Kiwi <contact@aikiwi.dev>
2026-08-27 16:45:08 +1200

    Added note about kernel page size not changeable

--- src/include/def.h
+++ src/include/def.h
@@ -10,6 +10,8 @@
 #define max_process_cnt 255
 #define max_pid_num 255
 
+// ths is defined here but is later not used in many places
+//  For this reason it is unsafe to change this.
 #define KERNEL_PAGE_SIZE 4096
 
 #define ROUND_MOD_DOWN(number, rounder) (((number) / (rounder)) * (rounder))
