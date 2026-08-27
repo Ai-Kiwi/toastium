529db5ce924a94efb9548d038f5c3fab84780586
Ai Kiwi <contact@aikiwi.dev>
2026-08-28 11:32:06 +1200

    Started work towards file descriptor process. Started work better naming scheme

---
+++ docs/naming_scheme.md
@@ -0,0 +1,44 @@
+This naming scheme was made a lot later in development so a lot doesn't follow it.
+Cost of learning a language and getting better at programming while making a project.
+
+Variables
+
+prefix:
+
+
+main content: 
+noun
+ - page
+ - addr
+ - process
+
+
+suffix
+modifier
+ - running
+ - cnt
+ - loc
+
+
+
+function names
+Local functions follow a similar system but they don't include the prefix (if adds clarity can be added.)
+global follow the rule of this
+
+prefix
+domain
+ - fd : file descriptor
+ - fs : file system
+ - proc : process
+ - vma : virtual memory allocator
+
+It will then follow with a verb for action being taken
+ - insert
+ - get
+ - remove
+
+After that will describe what it is doing.
+
+
+
+
--- src/kernel/syscall/file_system/handler.c
+++ src/kernel/syscall/file_system/handler.c
@@ -3,27 +3,51 @@
 #include "handler.h"
 #include "kernel/file_system/file_descriptor.h"
 #include "kernel/process/process.h"
+#include "types.h"
 
 static const u64 page_cnt = ((KERNEL_PAGE_SIZE / 8) - 1);
 
-static void *get_local_fd(u64 local_id, process *proc) {
+static file_descriptor *fd_get_local(u64 local_id, process *proc) {
     u64 page_num = local_id / page_cnt;
     u64 page_loc = local_id % page_cnt;
+
+    u64 *cur_page = proc->file_desc_pg;
+    for (u64 i = 0; i < page_num; i++) {
+        cur_page = (u64 *)cur_page[0];
+        if (cur_page == NULL) {
+            return NULL;
+        }
+    }
+
+    // if not set would already return null
+    return (file_descriptor *)cur_page[page_loc + 2];
 }
 
-static void insert_local_fd(file_descriptor *fd, process *proc) {
+static void fd_insert_local(file_descriptor *fd, process *proc) {
     // loop over all of them and find location.
     // Could possibly store last location that was freed for quicker access.
 }
 
-static void remove_local_fd(u64 local_id, process *proc) {
+static void fd_remove_local(u64 local_id, process *proc) {
     u64 page_num = local_id / page_cnt;
-    u64 page_loc = local_id % page_cnt;
+    u64 page_loc = (local_id % page_cnt) + 1; // offset 1
+
+    u64 *cur_page = proc->file_desc_pg;
+    for (u64 i = 0; i < page_num; i++) {
+        cur_page = (u64 *)cur_page[0];
+        if (cur_page == NULL) {
+            return;
+        }
+    }
+
+    if (cur_page[page_loc + 2] == 0) {
+        return;
+    }
+
+    cur_page[page_loc + 2] = 0;
 }
 
 // everything in this folder is only interuptable versions.
 // As such it isn't split up.
 
-void syscall_fs_open(trap_data *trap) {
-    //
-}
+void syscall_fs_open(trap_data *trap) {}
